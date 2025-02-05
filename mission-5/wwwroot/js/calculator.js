1950a7b862d2da6b9fa3d90aa4148e310c6a580c
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-02-05 11:47:40 -0700

    new commit should be final

--- mission-5/wwwroot/js/calculator.js
+++ mission-5/wwwroot/js/calculator.js
@@ -1,10 +1,16 @@
-﻿$("#calculate-btn").click(function () {
+﻿
+// This jquery grabs the calculate button on the calcualte page and when it is clicked activates the function below
+$("#calculate-btn").click(function () {
+    // this grabs the amount of hours put in by the user
     const hours = $("#hours").val()
 
+    //This grabs the rate which is a locked consistent rate of 25
     let rate = $("#rate").val()
 
+    // this multiplies the rate by the amount of hour inputted by the user
     let totalRate = hours * rate
 
+    // this inputs the total Rate into the total value
     $("#total").val(totalRate) 
 
 })
